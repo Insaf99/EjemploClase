9ec7d6d197753992e6a8e3e9c6afa0d23a76c45b
Franco Insaurralde <francoenzo1999@hotmail.com>
2022-02-04 15:05:51 -0300

    ultimo commit

--- src/main/java/com/example/ClaseSpring/EjemploClase/Service/PersonaServiceImpl.java
+++ src/main/java/com/example/ClaseSpring/EjemploClase/Service/PersonaServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.ClaseSpring.EjemploClase.Service;
 import com.example.ClaseSpring.EjemploClase.Dao.PersonaDao;
 import com.example.ClaseSpring.EjemploClase.Dominio.Persona;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,21 +13,25 @@ public class PersonaServiceImpl implements PersonaService{
     private PersonaDao personaDao;
 
     @Override
+    @Transactional(readOnly=true)
     public List<Persona> listarPersonas() {
         return (List<Persona>) personaDao.findAll();
     }
 
     @Override
+    @Transactional
     public void guardar(Persona persona) {
         personaDao.save(persona);
     }
 
     @Override
+    @Transactional
     public void eliminar(Persona persona) {
         personaDao.delete(persona);
     }
 
     @Override
+    @Transactional(readOnly=true)
     public Persona encontrarPersona(Persona persona) {
         return personaDao.findById(persona.getIdpersona()).orElse(null);
     }
